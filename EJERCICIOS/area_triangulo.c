319894da8ba7cc5676a7fd3cfe73ed16084a074b
DANIELA CASTILLO <DANIELA-CE@users.noreply.github.com>
2020-10-22 17:04:09 -0500

    Cambio de mensaje

--- EJERCICIOS/area_triangulo.c
+++ EJERCICIOS/area_triangulo.c
@@ -4,12 +4,16 @@ int main()
 {
 	int base,altura;
 	float area;	
-		printf ( "Da el valor de la base \n");
+	
+		printf ( "Asigna el valor de la base  en numeros enteros\n");
 		scanf("%d",&base);
-		printf ("Da el valor de la altura \n");
+		
+		printf ("Asigna el valor de la altura \n");
 		scanf("%d",&altura);
+		
 	area=((base*altura)/2);
 		printf("El area del triangulo con base %d y altura %d es: %0.2f\n",base, altura,area);
+		
 return 0;
 }
 
