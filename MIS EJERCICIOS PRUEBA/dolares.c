3a82e8f1ed41cd05bcdb0d3319d5a6fddbe9d65e
DANIELA CASTILLO <DANIELA-CE@users.noreply.github.com>
2020-10-22 20:46:14 -0500

    Update dolares.c

--- MIS EJERCICIOS PRUEBA/dolares.c
+++ MIS EJERCICIOS PRUEBA/dolares.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	float G,L,J;
+	float G,L,J,Total;
 		printf("Guillerno tiene N dolares.\n Luis tiene la mitad de lo que tiene Guillermo. \n Juan tiene la mitad de lo que posee Luis y Guillermo juntos \n");
 		printf("Cuantos dolares tiene Guillermo \?\n");
 		scanf("\v%f",&G);
@@ -12,5 +12,7 @@ int main()
 		J= (G+L)/2;
 		printf("La cantidad de dolares que tiene Juan es %0.2f\n",J);
 		
+		Total= G+L+J;
+		printf("Entre Guillemor, Luis y Juan juntan %0.2f dolares en total.\n",Total);
 	return 0;
 }
