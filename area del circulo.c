f74b39217e12f7dad162fa37672a5ba4cf2b5c82
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 12:50:32 -0600

    Update area del circulo.c
    Se quitaron paréntesis.

--- area del circulo.c
+++ area del circulo.c
@@ -7,8 +7,8 @@
 	float l, a;
 	printf("Dame el valor del radio (en numeros enteros):\n");
 	scanf("\n%d",&r);
-	l= ((2*PI)*r);
-	a = (PI*pow(r,2));
+	l= (2 * PI) * r;
+	a = PI * pow(r,2);
 	printf("La longuitud del circulo con radio %d es: %0.2f\n",r,l);
  	printf("El area del circulo con radio %d es: %0.2f", r, a);
      return 0;
